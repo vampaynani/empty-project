b9fbbc6abae99bd9e93bb8e59266c61875ab4854
Wenceslao Negrete <vampay@digitaldealers.mx>
2022-11-26 02:36:37 +0000

    add age

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ user = User()
 user.firstname = "John"
 user.lastname = "Doe"
 print(user.get_fullname())
+user.birth_year = 2000
+print(user.get_age())
 # John Doe
--- user.py
+++ user.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
 class User:
     firstname=""
     lastname=""
+    birth_year=0
 
     def get_fullname(self):
         return self.firstname+" "+self.lastname
+    
+    def get_age(self):
+        return datetime.now().year - self.birth_year
