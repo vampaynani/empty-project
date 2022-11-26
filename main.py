2c70ac3ab93445652be2ccb3e6c6c8699e4d4570
Wenceslao Negrete <vampay@digitaldealers.mx>
2022-11-26 02:47:06 +0000

    Agregue clase cat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from user import User
 from dog import Dog
+from cat import Cat
 
 user = User()
 user.firstname = "John"
@@ -8,5 +9,5 @@ print(user.get_fullname())
 user.birth_year = 2000
 print(user.get_age())
 # John Doe
-pet = Dog()
+pet = Cat()
 print(pet.type, pet.sound) # "dog", "barks"
